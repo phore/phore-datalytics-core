06bd2e646daccdac00dde46dc6d93f79a737a063
Matthias <m@tth.es>
2018-11-26 10:59:24 +0100

    removed version identifier for phpunit

---
+++ tests/unit/DataMergeTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthias
+ * Date: 12.11.18
+ * Time: 16:51
+ */
+
+namespace Test;
+
+
+use PHPUnit\Framework\TestCase;
+
+class DataMergeTest extends TestCase
+{
+
+
+    public function testDataMergeWithMultipleDataSources()
+    {
+        $source1 = new DataSource();
+        $source2 = new DataSource();
+
+        $merge = new DataMerge();
+        $merge->addSource($source1);
+        $merge->addSource($source2);
+
+
+        
+
+
+    }
+
+
+}
