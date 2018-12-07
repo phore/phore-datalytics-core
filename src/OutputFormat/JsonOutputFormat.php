f4fcf9ca288af030026eed9a5295fad09497b9ec
Jan <jan@talpa-solutions.com>
2018-12-07 15:00:01 +0100

    added string replace for Json Gps Coordinates

--- src/OutputFormat/JsonOutputFormat.php
+++ src/OutputFormat/JsonOutputFormat.php
@@ -39,17 +39,22 @@ class JsonOutputFormat implements OutputFormat
             $this->data = [];
         }
         if(empty($this->data)){
-            $this->data["Timestamp"] = $ts;
+            $this->data["timestamp"] = $ts;
         }
         foreach ($data as $key => $item) {
-            $this->data[$key] = $item;
+            $regex = '/{\s?pos:\s?\[(\d+\.\d+),(\d+\.\d+),(\d+\.\d+)\],\s?utc_time:\s?(\d+.\d+),\s?pos_mode:\s?(\d+),\s?tracked_sats:\s?(\d+),\s?hdop:\s?(\d+.\d+),\s?corr_age:\s?(\d+.\d+),\ssta_id:\s?(\d+)\s?}/i';
+
+            $replacer = '{"lat": $2, "lng": $1, "alt": $3, "utc_time": $4, "pos_mode": $5, "tracked_sats": $6, "hdop": $7, "corr_age": $8, "sta_id": $9}';
+
+            $item = preg_replace($regex, $replacer, $item);
+            $this->data[$key] = json_decode($item);
         }
         return true;
     }
 
     public function close()
     {
-        $this->outputJson["data"][] = $this->data;
+        $this->outputJson["data"][]= $this->data;
         echo json_encode($this->outputJson);
         return true;
     }
